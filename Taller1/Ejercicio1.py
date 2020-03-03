c1ce42965be5824b5f3dda0903d1df2b55600a0c
Emmanuel Arizabaleta <emma145@hotmail.es>
2020-03-02 22:42:43 -0500

    primer commit, no se ha avanzado el taller, solo se ha instalado todo el entorno

---
+++ Taller1/Ejercicio1.py
@@ -0,0 +1,12 @@
+""" Sean f y g dos números enteros de tres dígitoscada uno.
+ Realice un algoritmo que permita invertir los valores de los dígitos de g e intercambiar
+ el primer y segundo dígito de f, de tal forma que se muestre el resultado del intercambio 
+ de los dígitos, ademáse debe mostrar el resultadode la suma y la multiplicación
+ entre los números si f es mayor que gy si g es mayor que f se debe
+ mostrar la resta y la división de estos """ 
+
+print("Hello world")
+
+name = input("Ingrese su nombre: ")
+
+print("Su nombre es {0}".format(name))
