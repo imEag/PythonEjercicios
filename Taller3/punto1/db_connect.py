37aedb8cc91510848eb01b7ba5f1cb0842df7c61
Emmanuel Arizabaleta <emma145@hotmail.es>
2020-06-12 15:25:38 -0500

    Taller3: se terminó el punto 1 (se comentó el código)

--- Taller3/punto1/db_connect.py
+++ Taller3/punto1/db_connect.py
@@ -1,6 +1,6 @@
 
 import mysql.connector
-
+# Datos de inicio de sesión
 SERVER = 'localhost'
 USER = 'root'
 PASSWORD = ''
@@ -8,31 +8,39 @@ DB = 'taller3ej1'
 
 
 def get_user_information(user):
+    """ Recoje la info del usuario. Parámetro: Nombre de usuario (string). Retorna una lista """
     try:
+        #establece  conexión
         cnx = mysql.connector.connect(
         user=USER, password=PASSWORD, host=SERVER, database=DB)
         connection = cnx.cursor()
+        #query
         sql = "SELECT user.user_name, user.password FROM user WHERE user.user_name='%s'" % user
         connection.execute(sql)
         result = connection.fetchall()
-
+        #Si no hay dicho ususario retorna una lista vacía.
         if result == []:
             return ['']
         else:
             return result[0]
+        #Cierra conexión
         cnx.commit()
         cnx.close()
     except: 
         print('Ocurrió un error')
 
 def change_password(user_name, new_password):
+    """ Cambia la contraseña de un usuario. Parámetros: Nombre de usuario (Str), nueva password (string). No retorna"""
     try:
+        #Xonexión
         cnx = mysql.connector.connect(
         user=USER, password=PASSWORD, host=SERVER, database=DB)
         connection = cnx.cursor()
+        #query
         sql = "UPDATE user SET password=" + new_password + \
             " WHERE user_name='%s'" % user_name
         connection.execute(sql)
+        #Cierra conexión
         cnx.commit()
         cnx.close()
     except:
--- Taller3/punto1/main.py
+++ Taller3/punto1/main.py
@@ -2,31 +2,40 @@ from db_connect import get_user_information, change_password
  
 #--------------------------
 
+
 def loged_menu(user):
+    """ Menú de sesión iniciada. No retorna. Parámetro: Nombre de usuario (variable string) """
+    #Verificación de opción válida
     while True:
         print('1 - Cambiar contraseña\n2 - Cerrar Sesión')
         op_loged_menu = input(' : ')
+        #menu
         if op_loged_menu == '1':
+            #pide nueva password y llama la función del modulo.
             new_password = input('Ingrese la nueva contraseña: ')
             change_password(user, new_password)
             break
         elif op_loged_menu == '2':
             break
 
-
+#Programa
 print('--SISTEMA DE USUARIOS--')
 while True:
+    #menú
     op_menu = input('1 - Ingresar\n2 - Salir\n  : ')
     if op_menu == '1':
         print('Ingrese sus datos a continuación: ')
         while True:
+            #pide nombre de usuario, verifica que exista y trae la info.
             user = input('Nombre de usuario: ')
             user_info = get_user_information(user)
             if user == user_info[0]:
                 while True:
                     passw = input('Contraseña: ')
+                    #verifica la contraseña
                     if passw == user_info[1]:
                         print('Contraseña correcta')
+                        #llama la función del menú con sesión iniciada.
                         loged_menu(user_info[0])
                         break
                     else:
